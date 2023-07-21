88392c39f87848b0e1e33b1b305d962f911e7bba
basile <basile@smartb.city>
2023-07-21 17:39:43 +0200

    fix keycloak loginrequest

--- platform/web/packages/keycloak/src/keycloak-theme/keycloakRequest.ts
+++ platform/web/packages/keycloak/src/keycloak-theme/keycloakRequest.ts
@@ -1,16 +1,20 @@
 import { request } from "@smartb/g2"
 
 
-export const postForm = (url: string, body: any) => {
+export const postForm = async (url: string, body: any) => {
     const formData = new FormData()
     Object.keys(body).forEach((key) => {
         formData.append(key, body[key])
     })
-    const res = request({
+    const res = await request({
         url,
         method: "POST",
         formData,
-        returnType: "json"
+        //@ts-ignore
+        contentType: "application/x-www-form-urlencoded",
+        returnType: "text"
     })
-    return res
+    document.open();
+    document.write(res);
+    document.close()
 }
