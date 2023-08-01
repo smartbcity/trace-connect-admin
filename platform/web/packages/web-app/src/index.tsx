53b2e659fa2602bc37065793aad805908b740161
Basile <basile@smartb.city>
2023-08-01 17:13:01 +0200

    add oidc service worker

--- platform/web/packages/web-app/src/index.tsx
+++ platform/web/packages/web-app/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import {
   AppProvider,
-  KeycloakProvider
+  KeycloakProvider,
+  g2Config,
+  OidcSecure
 } from "@smartb/g2-providers";
 import { ThemeContextProvider } from "@smartb/g2-themes";
 import { Typography } from "@mui/material";
@@ -10,6 +12,7 @@ import { theme } from "Themes";
 import { QueryClient } from 'react-query'
 import { createRoot } from 'react-dom/client'
 import { AppRouter } from "App/routes";
+import { OidcConfiguration } from "@axa-fr/oidc-client";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,12 +27,27 @@ const container: HTMLElement = document.getElementById("root")
 
 const root = createRoot(container)
 
+const oidcConfiguration: OidcConfiguration = {
+  client_id: g2Config().keycloak.clientId,
+  redirect_uri: window.location.origin + '/authentication/callback',
+  silent_redirect_uri:
+    window.location.origin + '/authentication/silent-callback',
+  scope: 'openid',
+  authority: g2Config().keycloak.url + '/realms/' + g2Config().keycloak.realm,
+  service_worker_relative_url:'/OidcServiceWorker.js',
+  storage: localStorage,
+  service_worker_only:false,
+}
+
 root.render(
   <React.StrictMode //react strict mode must be here to avoid an infinite loop if placed above KeycloakProvider
   >
     {/* @ts-ignore */}
     <ThemeContextProvider theme={theme}>
-      <KeycloakProvider >
+      <KeycloakProvider 
+      configuration={oidcConfiguration}
+      >
+        <OidcSecure>
           <AppProvider
             languages={languages}
             queryClient={queryClient}
@@ -37,6 +55,7 @@ root.render(
           >
             <AppRouter />
           </AppProvider>
+          </OidcSecure>
       </KeycloakProvider>
     </ThemeContextProvider>
   </React.StrictMode>
