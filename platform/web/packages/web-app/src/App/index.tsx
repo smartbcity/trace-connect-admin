ff9ba0d6e2163915d34ab17dbb973532cc2c5fe4
basile <basile@smartb.city>
2023-10-02 10:49:36 +0200

    add correct role indicator in user menu summary

--- platform/web/packages/web-app/src/App/index.tsx
+++ platform/web/packages/web-app/src/App/index.tsx
@@ -1,4 +1,4 @@
-import { useExtendedAuth } from "components";
+import { RoleTargetValues, useExtendedAuth } from "components";
 import { useMenu, useUserMenu } from "./menu";
 import { useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
@@ -8,14 +8,15 @@ import { StandAloneAppLayout } from "@smartb/g2";
 export const App = () => {
   const { t, i18n } = useTranslation()
   const menu = useMenu(t)
-  const { service, keycloak } = useExtendedAuth()
+  const { service, keycloak, roles } = useExtendedAuth()
   const user = useMemo(() => service.getUser(), [service.getUser])
   const { loggedMenu, notLoggedMenu } = useUserMenu(keycloak.logout, keycloak.login, t)
 
   useEffect(() => {
     i18n.language.includes("fr") ? i18n.changeLanguage("fr") : i18n.changeLanguage("en")
   }, [i18n.language])
-  
+
+  const role = useMemo(() => roles?.find((role) => role.targets.includes(RoleTargetValues.user()) && user?.roles.includes(role.identifier)), [roles, user?.roles[0]])
 
   return (
     <StandAloneAppLayout
@@ -41,7 +42,7 @@ export const App = () => {
         currentUser: user ? {
           givenName: user.firstName ?? "",
           familyName: user.lastName ?? "",
-          role: "Admin"
+          role: role?.locale[i18n.language] ?? "",
         } : undefined,
         loggedMenu,
         notLoggedMenu
